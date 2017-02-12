fbde0ec13b9d05e9e467c9616cdc239b76c51069
chardmd <richard.dimalanta@gmail.com>
2017-02-12 14:38:45 +0800

    added mailTo functionality.

--- src/app/components/contact/contact.js
+++ src/app/components/contact/contact.js
@@ -5,9 +5,21 @@ angular
       controller: Contact
     });
 
+Contact.$inject = ['$scope', '$window'];
+
 /** @ngInject */
-function Contact($scope) {
+function Contact($scope, $window) {
+  var $ctrl = this;
+
+  var emailId = "richard.dimalanta@gmail.com";
+  var subject = "I got an awesome opportunity waiting for you.";
+  var message = "Hi Richard.";
+
   $scope.$on('contact', function () {
 
   });
+
+  $ctrl.sendMail = function () {
+    $window.open("mailto:" + emailId + "?subject=" + subject + "&body=" + message, "_self");
+  };
 }
