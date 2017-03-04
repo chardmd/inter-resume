5017998f155d97b3146f6df0f063e1c5223e632d
chardmd <richard.dimalanta@gmail.com>
2017-03-04 15:32:15 +1100

    change background color for homepage.

--- src/app/components/main/main.js
+++ src/app/components/main/main.js
@@ -10,7 +10,7 @@ Main.$inject = ['$scope', '$element', '$document'];
 /** @ngInject */
 function Main($scope, $element, $document) {
   var options = {
-    sectionsColor: ['#000000', '#fafafa', '#fafafa', '#fafafa', '#fafafa', '#fafafa'],
+    sectionsColor: ['#2A3A3F', '#fafafa', '#fafafa', '#fafafa', '#fafafa', '#fafafa'],
     menu: '#menu',
     paddingTop: '50px',
     css3: true,
